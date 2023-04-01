24b3a4b70df18af6d2c25611259126d28f870747
Paweł Siwoń <siwonpawel@gmail.com>
2023-04-01 18:15:17 +0200

    init commit

---
+++ src/main/java/com/github/siwonpawel/salesinfo/SalesinfoApplication.java
@@ -0,0 +1,13 @@
+package com.github.siwonpawel.salesinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesinfoApplication.class, args);
+	}
+
+}
